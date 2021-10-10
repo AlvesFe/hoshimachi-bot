5917a7c0a5958edc17c746b00371f42e8e199faf
Felipe Batista Alves <felipe.alves135@etec.sp.gov.br>
2021-10-10 03:02:05 -0300

    DEV: reestrutura no esquema de pastas

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,10 +4,10 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
+  const command = require(`./src/commands/${file}`);
   commands.push(command.data.toJSON());
 }
 
