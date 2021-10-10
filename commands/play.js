a555f46a8a62581cd0be85c09d7bf16a3fffe924
Felipe Batista Alves <felipe.alves135@etec.sp.gov.br>
2021-10-10 02:44:07 -0300

    STYLE: Refatorando codigo para melhor entendimento

--- commands/play.js
+++ commands/play.js
@@ -3,6 +3,34 @@ const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@d
 const yts = require('yt-search');
 const ytdl = require('ytdl-core');
 
+function isVideo (search) {
+	return search.type === 'video';
+}
+
+async function playMusic (voiceChannel, interaction, url) {
+	const connection = joinVoiceChannel({
+		channelId: voiceChannel.id,
+		guildId: voiceChannel.guild.id,
+		adapterCreator: interaction.member.guild.voiceAdapterCreator
+	});
+
+	const player = createAudioPlayer();
+	connection.subscribe(player);
+
+	const stream = ytdl(url, {
+		filter: 'audioonly',
+		format: 'ogg',
+		highWaterMark: 1048576 * 32
+	})
+		.on('error', (e) => {
+			console.error(e);
+		});
+
+	const resource = createAudioResource(stream, { seek: 0, volume: 1 });
+	player.play(resource);
+}
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
@@ -12,41 +40,19 @@ module.exports = {
 				.setDescription('Link ou nome da música')
 				.setRequired(true)),
 	async execute (interaction) {
-		const search = await yts(interaction.options._hoistedOptions[0].value);
 		const user = await interaction.member.fetch();
 		const voiceChannel = await user.voice.channel;
+		const search = (await yts(interaction.options._hoistedOptions[0].value)).all[0];
 
 		if (!voiceChannel) {
-			await interaction.reply('Você precisa estar em um canal de voz para executar esse comando');
-		} else {
-			const url = search.all[0].url;
-			const connection = joinVoiceChannel({
-				channelId: voiceChannel.id,
-				guildId: voiceChannel.guild.id,
-				adapterCreator: interaction.member.guild.voiceAdapterCreator
-			});
-
-			const player = createAudioPlayer();
-			connection.subscribe(player);
-
-			const stream = ytdl(url, {
-				filter: 'audioonly',
-				format: 'ogg',
-				highWaterMark: 1048576 * 32
-			})
-				.on('error', (e) => {
-					console.error(e);
-				});
-
-
-			const resource = createAudioResource(stream, { seek: 0, volume: 1 });
-			player.play(resource);
-
-			// setTimeout(() => {
-			// 	connection.destroy();
-			// }, 5000);
-
-			await interaction.reply(`Tocando: ${search.all[0].title}\n Autor: ${search.all[0].author.name}`);
+			return await interaction.reply('Você precisa estar em um canal de voz para executar esse comando');
 		}
-	},
+
+		if (isVideo(search)) {
+			await playMusic(voiceChannel, interaction, search.url);
+			await interaction.reply(`Tocando: ${search.title}\n Autor: ${search.author.name}`);
+		} else {
+			await interaction.reply('Música não encontrada');
+		}
+	}
 };
