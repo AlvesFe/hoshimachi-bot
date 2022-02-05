dc828330a56f97d976059119b22951f46b6ef342
Felipe Batista <lipe.batistao@live.com>
2022-02-05 18:59:34 -0300

    DOCS: Adicionado jsdoc ao comando de leave

--- src/commands/leave.js
+++ src/commands/leave.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+const { Interaction, User, VoiceChannel } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getVoiceConnection } = require('@discordjs/voice');
 const { queue } = require('../resources');
@@ -17,8 +19,15 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('leave')
 		.setDescription('Sai do canal de voz e remove a música'),
+	/**
+	 * Executa o comando
+	 * @param {Interaction} interaction Interaction do comando
+	 * @returns {void}
+	 */
 	async execute (interaction) {
+		/** @type {User} */
 		const user = await interaction.member.fetch();
+		/** @type {VoiceChannel} */
 		const voiceChannel = await user.voice.channel;
 		const connection = getVoiceConnection(voiceChannel && voiceChannel.guild.id);
 
